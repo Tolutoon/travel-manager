34413d089d26a849d1d8b59a2075b64f9e6f8b59
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-09-01 12:25:15 +0100

    final commit

---
+++ src/Data.jsx
@@ -0,0 +1,40 @@
+const Data = [
+    {
+        id: 1,
+        name: 'Olopade Tolu',
+        email: 'getolopadetolu@gmail.com',
+        phone: '08169580103',
+        address: 'Yaba, Lagos.' 
+    },
+    {
+        id: 2,
+        name: 'Emily Johnson',
+        email: 'emily.johnson@example.com',
+        phone: '(555) 123-4567',
+        address: 'Paris, France' 
+    },
+    {
+        id: 3,
+        name: 'Benjamin Smith',
+        email: 'benjamin.smith@example.com',
+        phone: '(555) 123-4567',
+        address: 'Tokyo, Japan' 
+    },
+    {
+        id: 4,
+        name: 'Chloe Davis',
+        email: 'chloe.davis@example.com',
+        phone: '(555) 123-4567',
+        address: 'New York, USA.' 
+    },
+    {
+        id: 5,
+        name: 'William Brown',
+        email: 'william.brown@example.com',
+        phone: '(555) 123-4567',
+        address: 'Sydney Australia' 
+    },
+
+]
+
+export default Data;
--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import '../styles/client.css'
+import React, { useState } from "react";
+import Data from "../Data";
+import "../styles/client.css";
 
 function Client() {
+  const [contacts, setContacts] = useState(Data);
   return (
-    <div className='class-container'>
-        <h3>Clients</h3>
+    <div className="class-container">
+      <div className="client-text">
+        <h1>Clients</h1>
+        <p>Here's the client list</p>
+      </div>
+      <div className="table-box">
+        <div style={{marginBottom: 64}}>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Search by name, and email..."
+        />
+        <button className="client-btn">Add New Client</button>
+        </div>
+        <table>
+          <thead>
+            <tr className="t-head">
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((elem) => {
+              return (
+              <tr>
+                <td>{elem.name}</td>
+                <td>{elem.email}</td>
+                <td>{elem.phone}</td>
+                <td>{elem.address}</td>
+              </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Client
+export default Client;
